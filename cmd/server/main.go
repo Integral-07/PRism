c41438e145bdc8d3bc9e7e870bab178f4e50e926
tk.manato <tk.07@outlook.jp>
2026-05-24 18:55:53 +0900

    [add] webhook endpoint (handler returns only statusOk)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+
+	"github.com/Integral-07/prism/internal/handler/webhook"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	webhookHandler := webhook.NewHandler()
+	r.Post("/webhook", webhookHandler.Handle)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("server starting on :%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
---
+++ internal/handler/webhook/handler.go
@@ -0,0 +1,13 @@
+package webhook
+
+import "net/http"
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
